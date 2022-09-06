b79d257a6b4c47e4ce2f41bd53a0ae470d6271f7
Alekasndr Dombradi <deviljin95@mail.ru>
2022-09-06 11:53:25 +0700

    vtb_cargo adaptive

--- vtb_cargo/app/js/main.js
+++ vtb_cargo/app/js/main.js
@@ -8,11 +8,11 @@ function resizeInput() {
 // функция валидации имени
 
 function formNameValid(el, elCheck) {
-    if(el.val().length < 2) {
+    if (el.val().length < 2) {
         elCheck = false;
         el.addClass('validate-border');
         el.parent().find('.msg-error').show();
-    } else  if (el.val().length === 0){
+    } else if (el.val().length === 0) {
         elCheck = false;
         el.parent().find('.msg-error').hide();
         el.parent().find('.msg-empty').show();
@@ -25,6 +25,7 @@ function formNameValid(el, elCheck) {
 
     return elCheck;
 }
+
 // функция валидации телефона
 
 function formPhoneValid(el, elCheck) {
@@ -54,6 +55,7 @@ function formPhoneValid(el, elCheck) {
 
     return elCheck;
 }
+
 // функция валидации email
 
 function formEmailValid(el, elCheck) {
@@ -61,7 +63,7 @@ function formEmailValid(el, elCheck) {
         emailValid = emailVal.split('@');
 
     if (el.val().length > 0 && (el.val().match(/.+?\@.+\.+/g) || []).length === 1) {
-        if(emailValid[0].length < 3) {
+        if (emailValid[0].length < 3) {
             el.addClass('validate-border');
             el.parent().find('.msg').hide();
             el.parent().find('.msg-error-length').show();
@@ -84,6 +86,26 @@ function formEmailValid(el, elCheck) {
 $(document).ready(function () {
 @@include('_webp_config.js');
 
+    $('.hamburger').on('click', function () {
+        $(this).toggleClass('open');
+        $('header').find('.nav-menu').toggleClass('show');
+    });
+
+    $(document).on('click', '.catalog__btn-show-filters', function () {
+        if ($(this).text().trim() === "Показать фильтры") {
+            $(this).text("Скрыть фильтры");
+        } else {
+            $(this).text("Показать фильтры");
+        }
+
+        $('.catalog').find('.filters').toggleClass('show');
+    });
+
+    $(document).on('click', '.catalog .filters .close', function () {
+        $(this).closest('.filters').removeClass('show');
+        $('.catalog__btn').text("Показать фильтры");
+    })
+
     $('.main-hero__slider').slick({
         arrows: false,
         infinite: true,
@@ -94,6 +116,12 @@ $(document).ready(function () {
         autoplaySpeed: 2000
     });
 
+    if (window.matchMedia('(max-width: 767.98px)').matches) {
+        var mainHeight = $('.main-hero').outerHeight();
+
+        $('.main-hero__slider-item').css('height', mainHeight + 'px')
+    }
+
     customSelect('.select', '.select-title', '.select-content', '.select-content__wrapper', '.select-content__radio', 4) // init кастомного селекта
 
     clickOutside('.select', '.select-content', 'open') // клик вне элемента
@@ -101,15 +129,32 @@ $(document).ready(function () {
     function catalogSlider(el) {
         console.log(el)
         $(el).slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             speed: 300,
             draggable: false,
             arrows: true,
             infinite: true,
+            variableWidth: true,
             prevArrow: '<a class="arrow-left"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.5 0.884978C9.27834 0.884978 9.05667 0.966644 8.88167 1.14164L1.275 8.74831C0.0383371 9.98498 0.0383371 12.015 1.275 13.2516L8.88167 20.8583C9.22 21.1966 9.78 21.1966 10.1183 20.8583C10.4567 20.52 10.4567 19.96 10.1183 19.6216L2.51167 12.015C1.95167 11.455 1.95167 10.545 2.51167 9.98498L10.1183 2.37831C10.4567 2.03998 10.4567 1.47998 10.1183 1.14164C9.94334 0.978311 9.72167 0.884978 9.5 0.884978Z" fill="currentColor"/></svg></a>',
             nextArrow: '<a class="arrow-right"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.50003 0.884978C1.72169 0.884978 1.94336 0.966644 2.11836 1.14164L9.72503 8.74831C10.9617 9.98498 10.9617 12.015 9.72503 13.2516L2.11836 20.8583C1.78003 21.1966 1.22003 21.1966 0.881695 20.8583C0.543362 20.52 0.543362 19.96 0.881695 19.6216L8.48836 12.015C9.04836 11.455 9.04836 10.545 8.48836 9.98498L0.881695 2.37831C0.543362 2.03998 0.543362 1.47998 0.881695 1.14164C1.05669 0.978311 1.27836 0.884978 1.50003 0.884978Z" fill="currentColor"/></svg></a>',
-            swipe: true
+            swipe: true,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
     }
 
